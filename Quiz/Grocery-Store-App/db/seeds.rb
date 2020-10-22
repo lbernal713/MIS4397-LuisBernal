4a8809c05f7d77f93fad66f28a1832eae0c3034e
Ubuntu <ubuntu@ip-172-31-23-219.us-east-2.compute.internal>
2020-10-22 05:13:49 +0000

    Commited bootstrap changes done to the page

--- Quiz/Grocery-Store-App/db/seeds.rb
+++ Quiz/Grocery-Store-App/db/seeds.rb
@@ -7,18 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 4.times do |i|
-  Store.create(name: "#{Faker::Company.name} Grocery Store", description: Faker::Restaurant.description, url: Faker::Internet.url, status: Faker::Subscription.status, code: Faker::IDNumber.valid, address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip, country: Faker::Address.country)
+  Store.create(name: Faker::Company.name, description: Faker::Restaurant.description, url: Faker::Internet.url, status: Faker::Subscription.status, code: Faker::IDNumber.valid, address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip, country: Faker::Address.country)
 end
 
 4.times do |i|
-  Department.create(store: Faker::IDNumber.valid, name: "#{Faker::Company.name} Grocery Store", description: Faker::Restaurant.description, code: Faker::IDNumber.valid)
+  Department.create(store_id: @store_id, name: Faker::Company.name, description: Faker::Restaurant.description, code: Faker::IDNumber.valid, store: @store_id)
 end
 
 4.times do |i|
-  Employee.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Company.buzzword, role: Faker::Job.title, status: Faker::Subscription.status, )
+  Employee.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Company.buzzword, role: Faker::Job.title, status: Faker::Subscription.status, department: @department_id)
 end
 
 4.times do |i|
-  Review.create(note: Faker::ChuckNorris.fact)
+  Review.create(note: Faker::ChuckNorris.fact, store: @store_id)
 end
 
